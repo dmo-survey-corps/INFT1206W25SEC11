cbc1af208185eeaaaad65f93c865e3da3620f508
dmo-survey-corps <redoyeyemi685@gmail.com>
2025-03-12 16:25:05 -0400

    Update main.js

--- assignment4/part1/main.js
+++ assignment4/part1/main.js
@@ -5,7 +5,6 @@
     Description: This JS code will switch different measurments between UK and US measurement scales.
 */
 
-
 console.log("Hello");
 
 let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
