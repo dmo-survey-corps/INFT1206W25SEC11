fbbf69f707231370e222b230ce82f13b9b1e6ce4
dmo-survey-corps <redoyeyemi685@gmail.com>
2025-03-12 16:14:40 -0400

    comments added

--- assignment4/part1/main.js
+++ assignment4/part1/main.js
@@ -1,3 +1,11 @@
+/*
+    Name: David Oyeyemi
+    Student Number: 100987382
+    Date: Mar 12th, 2025
+    Description: This JS code will switch different measurments between UK and US measurement scales.
+*/
+
+
 console.log("Hello");
 
 let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
