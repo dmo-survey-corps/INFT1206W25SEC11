d7be56cddcf1df7a58167439c5889f46a6b6d04a
dmo-survey-corps <redoyeyemi685@gmail.com>
2025-04-04 21:38:24 -0400

    Button Event Listener added

--- assignment4/part2/main.js
+++ assignment4/part2/main.js
@@ -17,13 +17,12 @@ const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
 const altTexts = {
-    'pic1.jpg': 'Closeup of a human eye',
-    'pic2.jpg': 'Rock that looks like a wave',
-    'pic3.jpg': 'Purple and white pansies',
-    'pic4.jpg': 'Section of wall from a pharaoh\'s tomb',
-    'pic5.jpg': 'Large moth on a leaf'
+    "pic1.jpg" : "Closeup of a human eye",
+    "pic2.jpg" : "Rock that looks like a wave",
+    "pic3.jpg" : "Purple and white pansies",
+    "pic4.jpg" : "Section of wall from a pharaoh\'s tomb",
+    "pic5.jpg" : "Large moth on a leaf"
   };
-
 
 /* Looping through images */
 
@@ -33,3 +32,14 @@ newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+    const currentClass = btn.getAttribute('class');
+    if (currentClass === 'dark') {
+      btn.setAttribute('class', 'light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    } else {
+      btn.setAttribute('class', 'dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)';
+    }
